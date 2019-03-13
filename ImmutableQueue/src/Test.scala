dad4a19f792e2e5233ce284cc89c1dda751e21da
Joseph Orme <brogramn@mercari.com>
2019-03-13 10:37:51 +0900

    Add files via upload

--- ImmutableQueue/src/Test.scala
+++ ImmutableQueue/src/Test.scala
@@ -1,7 +1,17 @@
 import Queue.ImmutableQueue
 import Stack.EmptyStack
+import Queue.EmptyQueue
 
 object Test extends App {
-   var queue = new ImmutableQueue[Int](new EmptyStack[Int], new EmptyStack[Int])
-   queue.enqueue()
+   var queue = new EmptyQueue[Int]
+   queue.enqueue(1)
+   queue.enqueue(2)
+   queue.enqueue(3)
+   queue.enqueue(4)
+   queue.enqueue(5)
+
+   while(!queue.isEmpty) {
+      System.out.println(queue.head)
+      queue = queue.dequeue()
+   }
 }
